3bf82bdc5b32f4ecfc48980c309e8b749e5d1f94
Francesco Occhipinti <f.occhipinti@gmail.com>
2012-06-18 22:17:30 +0200

    Other cleaning, unifyed different tornado scripts

--- setup.py
+++ setup.py
@@ -7,17 +7,5 @@ setup(
     install_requires='CherryPy',
     scripts=[
         'scripts/tornado',
-        'scripts/bounded-tornado',
-        'scripts/onepage-tornado',
         ],
-    entry_points = {
-        'console_scripts':[
-            'vis-parallel = vishnje:parallel',
-            'vis-splom = vishnje:splom',
-            'vis-crimea_stacked_area = vishnje:crimea_stacked_area',
-            'vis-zoom = vishnje:zoom',
-            'vis-marimekko = vishnje:marimekko',
-            'vis-bar = vishnje:bar',
-            ],
-    }
     )
