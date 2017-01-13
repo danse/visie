05f38fcd089c2f1924ab16b15f92638499bda0fd
danse <f.occhipinti@gmail.com>
2017-01-13 14:50:24 +0100

    use better types for the time series logic, closes danse/meta#17

--- src/Vishnje/ToTimeSeries.hs
+++ src/Vishnje/ToTimeSeries.hs
@@ -1,19 +1,18 @@
 module Vishnje.ToTimeSeries where
 
-import Data.DateTime( DateTime )
-import Data.List( sortOn )
-import Data.Time.Clock( NominalDiffTime,addUTCTime )
-import Data.Ord( compare )
+import Data.DateTime (DateTime)
+import Data.List (sortOn)
+import Data.Time.Clock (NominalDiffTime,addUTCTime)
+import Data.Ord (compare)
+import Data.Time (UTCTime)
 
-class Monoid a => Timeserializable a where
-  getTime :: a -> DateTime
-  setTime :: DateTime -> a -> a
+data Timestamped a = Timestamped { getStamped :: a, getTime :: UTCTime }
 
-merge :: Timeserializable a => DateTime -> a -> a -> a
-merge t a b = setTime t (mappend a b)
+merge :: Monoid a => DateTime -> Timestamped a -> Timestamped a -> Timestamped a
+merge t (Timestamped a _) (Timestamped b _) = Timestamped (mappend a b) t
 
-fill :: Timeserializable a => DateTime -> a
-fill t = setTime t mempty
+fill :: Monoid a => DateTime -> Timestamped a
+fill t = Timestamped mempty t
 
 -- at every call, the recursive function returns a processed list, and
 -- it gets a non processed list and a reference date about the last
@@ -22,7 +21,7 @@ fill t = setTime t mempty
 -- created. otherwise, the function will look ahead and merge all
 -- elements within the same interval, pick a representative date for
 -- the merged elements and use it as the new reference
-consume :: Timeserializable a => [DateTime] -> [a] -> [a]
+consume :: Monoid a => [DateTime] -> [Timestamped a] -> [Timestamped a]
 consume (t:ts) [] = []
 consume (t:ts) elements
   | length preceding == 0 = (fill t) : rest
@@ -33,13 +32,13 @@ consume (t:ts) elements
 iterator :: NominalDiffTime -> DateTime -> [DateTime]
 iterator interval start = iterate (addUTCTime interval) start
 
-convert :: Timeserializable a => NominalDiffTime -> [a] -> [a]
+convert :: Monoid a => NominalDiffTime -> [Timestamped a] -> [Timestamped a]
 convert interval elements =
   sampler sorted
   where sorted = sortOn getTime elements
         times = iterator interval (getTime (head sorted))
         sampler = consume times
 
-convertFill :: Timeserializable a => DateTime -> NominalDiffTime -> [a] -> [a]
+convertFill :: Monoid a => DateTime -> NominalDiffTime -> [Timestamped a] -> [Timestamped a]
 convertFill dateTime interval elements = convert interval filledElements
   where filledElements = (fill dateTime):elements
