030bfe060cab945c82eb18d43d56f7c7e9331796
danse <f.occhipinti@gmail.com>
2022-12-03 14:32:28 +0100

    remove dependency from datetime

--- src/Visie/ToTimeSeries.hs
+++ src/Visie/ToTimeSeries.hs
@@ -1,6 +1,5 @@
 module Visie.ToTimeSeries where
 
-import Data.DateTime (DateTime)
 import Data.List (sortOn)
 import Data.Time.Clock (NominalDiffTime,addUTCTime)
 import Data.Ord (compare)
@@ -8,7 +7,7 @@ import Data.Time (UTCTime)
 
 data Timestamped a = Timestamped { getStamped :: a, getTime :: UTCTime }
 
-fill :: Monoid a => DateTime -> Timestamped a
+fill :: Monoid a => UTCTime -> Timestamped a
 fill t = Timestamped mempty t
 
 -- | sort `elements` concatenating their monoid when their time falls
@@ -18,7 +17,7 @@ convert interval elements = sampler sorted
   where sorted = sortOn getTime elements
         times = iterator interval (getTime (head sorted))
         sampler = consume times
-        iterator :: NominalDiffTime -> DateTime -> [DateTime]
+        iterator :: NominalDiffTime -> UTCTime -> [UTCTime]
         iterator interval start = iterate (addUTCTime interval) start
         -- at every call, the recursive function returns a processed list, and
         -- it gets a non processed list and a reference date about the last
@@ -27,7 +26,7 @@ convert interval elements = sampler sorted
         -- created. otherwise, the function will look ahead and merge all
         -- elements within the same interval, pick a representative date for
         -- the merged elements and use it as the new reference
-        consume :: Monoid a => [DateTime] -> [Timestamped a] -> [Timestamped a]
+        consume :: Monoid a => [UTCTime] -> [Timestamped a] -> [Timestamped a]
         consume (t:ts) [] = []
         consume (t:ts) elements
           | length preceding == 0 = filled : rest
@@ -35,9 +34,9 @@ convert interval elements = sampler sorted
           where (preceding, succeeding) = span ((<= t) . getTime) elements
                 filled = fill t
                 rest = consume ts succeeding
-                merge :: Monoid a => DateTime -> Timestamped a -> Timestamped a -> Timestamped a
+                merge :: Monoid a => UTCTime -> Timestamped a -> Timestamped a -> Timestamped a
                 merge t (Timestamped a _) (Timestamped b _) = Timestamped (mappend a b) t
 
-convertFill :: Monoid a => DateTime -> NominalDiffTime -> [Timestamped a] -> [Timestamped a]
+convertFill :: Monoid a => UTCTime -> NominalDiffTime -> [Timestamped a] -> [Timestamped a]
 convertFill dateTime interval elements = convert interval filledElements
   where filledElements = (fill dateTime):elements
